eab3dec1824598776ca6453e1bce06c862e230a1
Salihcan Kahya <salihcankahya@gmail.com>
2020-07-27 23:28:25 +0300

    Details view added && duel button and push navigator added

---
+++ lib/Views/game_room_page/game_room_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import './game_room_page_view.dart';
+
+class GameRoomPage extends StatefulWidget {
+  
+  @override
+  GameRoomPageView createState() => new GameRoomPageView();
+}
+  
